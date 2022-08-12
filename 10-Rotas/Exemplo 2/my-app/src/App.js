48144d139178caae5d2328261f9d0827ab2642e4
Ariele Ketely <arieleketely2012@hotmail.com>
2022-08-12 10:51:51 -0300

    Update App.js

--- 10-Rotas/Exemplo 2/my-app/src/App.js
+++ 10-Rotas/Exemplo 2/my-app/src/App.js
@@ -1,23 +1,31 @@
-
-import {BrowserRouter as Router, Routes ,Route} from 'react-router-dom';
-import './App.css';
-
+import {BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './Pages/Home';
 import Sobre from './Pages/Sobre';
 import Usuario from './Pages/Usuario';
-import Navbar from './Pages/Navbar';
-export default function App() {
-   return (
-<>
-    <h1>Rotas</h1>
-       
-   <Navbar/>
-         <Routes>
-          <Route path="/" element={<Home/>} />
+
+
+function App() {
+  return (
+   
+    <Router>
+    <>
+      <ul>
+        <li> <Link to='/'>Home</Link> </li>
+        <li> <Link to='/sobre'>Sobre</Link> </li>
+        <li> <Link to='/usuario'>Usuario</Link> </li>
+      </ul>
+    <Routes>
+        <Route path='/' element={<Home />} />
+      
           <Route path="/sobre" element={<Sobre/>} />
           <Route path="/usuario" element={<Usuario/>} />
-</Routes>
-       
-       </>
-   );
+         
+      
+      </Routes>
+    </>
+    </Router>
+  );
 }
+ 
+export default App;
+
